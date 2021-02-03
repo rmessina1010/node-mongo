6b8f87b4c7b63ee2d6712a268488c490eeb3677b
Ray Messina <rmessina10@gmail.com>
2021-02-03 00:14:21 -0600

    Node MongoDB Example 1

---
+++ index.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert').strict;
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'nucampsite';
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+
+    assert.strictEqual(err, null);
+
+    console.log('Connected correctly to server.');
+    const db = client.db(dbname);
+
+    db.dropCollection('campsites', (err, result) => {
+        assert.strictEqual(err, null);
+        console.log('Dropped Collection', result);
+
+        const collection = db.collection('campsites');
+        console.log(collection);
+        collection.insertOne(
+            {
+                name: "Bread Crumb Trail",
+                description: "Test"
+            },
+            (err, result) => {
+                assert.strictEqual(err, null);
+                console.log('Insert Document:', result.ops);
+
+                collection.find().toArray((err, docs) => {
+                    assert.strictEqual(err, null);
+                    console.log(docs);
+                    client.close();
+                });
+            }
+
+        );
+
+    });
+});
